6a8330d976eeda32e36f51f41f5342fd260ce88a
Siree77 <billsire957@gmail.com>
2024-09-27 18:37:42 +0100

    Organize project structure, add initial files and images

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Custom JavaScript can be added here
+
+// Example: Smooth scroll for older browsers
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
